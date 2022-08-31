381c1a18b1ca378b1d5386263a2522883e807b7e
denisbeslic <denis.beslic@outlook.com>
2022-08-31 17:11:05 +0200

    Restructure preprocessing, add logger, remove output parameter

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,18 @@
 import csv
 import re
+import logging
+import logging.config
+import os
+
+logger = logging.getLogger(__name__)
 
 
 def denovo_setup():
     print("Function denovo_setup was called.")
     # TODO: Write function to automatically download all necessary models, knapsack files, test data.
 
-'''
+
+"""
 Function for formatting MGF file is taken from PostNovo
 
 Title: PostNovo
@@ -14,72 +20,99 @@ Author: SE Miller / semiller10
 Date: 14.Aug. 2019
 Version: 1.0.9-alpha
 Link: https://github.com/semiller10/postnovo/blob/988b728fad96815fbe94a2bd4dc79ec3b417f099/userargs.py#L1558
-'''
-def reformat_MGF(mgf_in, mgf_out):
-    print("Function reformat_MGF was called.")
-    print(mgf_in)
-    print(mgf_out)
+"""
+
+
+def reformat_MGF(mgf_in):
+    mgf_out = mgf_in.replace(".mgf", "_reformatted.mgf")
     new_index = 1
     new_scan = 1
-    with open(mgf_in) as in_f, open(mgf_out, 'w') as out_f, open(mgf_out.replace(".mgf", "_deepnovo.mgf"),
-                                                                 'w') as out_f2:
+    with open(mgf_in) as in_f, open(mgf_out, "w") as out_f, open(
+        mgf_out.replace(".mgf", "_deepnovo.mgf"), "w"
+    ) as out_f2:
         for line in in_f:
-            if line == 'BEGIN IONS\n':
+            if line == "BEGIN IONS\n":
                 ms2_peak_lines = []
-            elif 'TITLE=' == line[:6]:
-                split_line = line.split('TITLE=Run: ')[1]
-                run_id, split_line = split_line.split(', Index: ')
-                old_index, split_line = split_line.split(', Scan: ')
-                old_scan = split_line.rstrip('\n')
-            elif 'PEPMASS=' == line[:8]:
+            elif "TITLE=" == line[:6]:
+                split_line = line.split("TITLE=Run: ")[1]
+                run_id, split_line = split_line.split(", Index: ")
+                old_index, split_line = split_line.split(", Scan: ")
+                old_scan = split_line.rstrip("\n")
+            elif "PEPMASS=" == line[:8]:
                 # Remove intensity data: DeepNovo only looks for the mass and not intensity.
-                if ' ' in line:
-                    pepmass_line = line.split(' ')[0] + '\n'
+                if " " in line:
+                    pepmass_line = line.split(" ")[0] + "\n"
                 else:
                     pepmass_line = line
-            elif 'CHARGE=' == line[:7]:
+            elif "CHARGE=" == line[:7]:
                 charge_line = line
-            elif 'RTINSECONDS=' == line[:12]:
+            elif "RTINSECONDS=" == line[:12]:
                 rt_line = line
-            elif 'SEQ=' == line[:4]:
+            elif "SEQ=" == line[:4]:
                 seq_line = line
-            elif line == 'END IONS\n':
+            elif line == "END IONS\n":
                 # Avoid peptides without MS2 peaks.
                 if len(ms2_peak_lines) > 0:
-                    out_f.write('BEGIN IONS\n')
+                    out_f.write("BEGIN IONS\n")
                     out_f.write(
-                        'TITLE=Run: ' + run_id + ', Index: ' + str(new_index) + \
-                        ', Old index: ' + old_index + ', Old scan: ' + old_scan + '\n')
+                        "TITLE=Run: "
+                        + run_id
+                        + ", Index: "
+                        + str(new_index)
+                        + ", Old index: "
+                        + old_index
+                        + ", Old scan: "
+                        + old_scan
+                        + "\n"
+                    )
                     out_f.write(pepmass_line)
                     out_f.write(charge_line)
-                    out_f.write('SCANS=' + str(new_scan) + '\n')
+                    out_f.write("SCANS=" + str(new_scan) + "\n")
                     out_f.write(rt_line)
-                    out_f.write(''.join(ms2_peak_lines))
+                    out_f.write("".join(ms2_peak_lines))
                     out_f.write(line)
 
-                    out_f2.write('BEGIN IONS\n')
+                    out_f2.write("BEGIN IONS\n")
                     out_f2.write(
-                        'TITLE=Run: ' + run_id + ', Index: ' + str(new_index) + \
-                        ', Old index: ' + old_index + ', Old scan: ' + old_scan + '\n')
+                        "TITLE=Run: "
+                        + run_id
+                        + ", Index: "
+                        + str(new_index)
+                        + ", Old index: "
+                        + old_index
+                        + ", Old scan: "
+                        + old_scan
+                        + "\n"
+                    )
                     out_f2.write(pepmass_line)
                     out_f2.write(charge_line)
-                    out_f2.write('SCANS=' + str(new_scan) + '\n')
+                    out_f2.write("SCANS=" + str(new_scan) + "\n")
                     out_f2.write(rt_line)
                     out_f2.write("SEQ=AAAAAAA\n")
-                    out_f2.write(''.join(ms2_peak_lines))
+                    out_f2.write("".join(ms2_peak_lines))
                     out_f2.write(line)
                     new_index += 1
                     new_scan += 1
             else:
                 ms2_peak_lines.append(line)
+    logger.info(f"Reformatted file can be found in {os.path.abspath(mgf_out)}")
     return
 
 
 def transfer_mgf(old_mgf_file_name, output_feature_file_name, spectrum_fw):
-    with open(old_mgf_file_name, 'r') as fr:
-        with open(output_feature_file_name, 'w') as fw:
-            writer = csv.writer(fw, delimiter=',')
-            header = ["spec_group_id", "m/z", "z", "rt_mean", "seq", "scans", "profile", "feature area"]
+    with open(old_mgf_file_name, "r") as fr:
+        with open(output_feature_file_name, "w") as fw:
+            writer = csv.writer(fw, delimiter=",")
+            header = [
+                "spec_group_id",
+                "m/z",
+                "z",
+                "rt_mean",
+                "seq",
+                "scans",
+                "profile",
+                "feature area",
+            ]
             writer.writerow(header)
             flag = False
             for line in fr:
@@ -95,7 +128,7 @@ def transfer_mgf(old_mgf_file_name, output_feature_file_name, spectrum_fw):
                     spectrum_fw.write(line)
                 elif line.startswith("CHARGE="):
                     z = re.split("=|\r|\n|\+", line)[1]
-                    spectrum_fw.write("CHARGE=" + z + '\n')
+                    spectrum_fw.write("CHARGE=" + z + "\n")
                 elif line.startswith("SCANS="):
                     scan = re.split("=|\r|\n", line)[1]
                     spectrum_fw.write(line)
@@ -105,7 +138,9 @@ def transfer_mgf(old_mgf_file_name, output_feature_file_name, spectrum_fw):
                 elif line.startswith("SEQ="):
                     seq = re.split("=|\r|\n", line)[1]
                 elif line.startswith("END IONS"):
-                    feature = Feature(spec_id=scan, mz=mz, z=z, rt_mean=rt_mean, seq=seq, scan=scan)
+                    feature = Feature(
+                        spec_id=scan, mz=mz, z=z, rt_mean=rt_mean, seq=seq, scan=scan
+                    )
                     writer.writerow(feature.to_list())
                     flag = False
                     del scan
--- src/user_args.py
+++ src/user_args.py
@@ -7,98 +7,178 @@ from denovo import denovo_seq
 
 logger = logging.getLogger(__name__)
 
+
 def setup(test_argv=None):
     parser = argparse.ArgumentParser(
-        description='De Novo Sequencing Pipeline for peptide prediction and assembly of full-protein sequences by'
-                    'tandem mass spectrometry.')
-    subparsers = parser.add_subparsers(dest='subparser_name')
-    reformatMGF_parser = subparsers.add_parser('reformatMGF',
-                                               help='Formatting MGF files to be compatible with all de novo tools. Indices are changed from 0 to N.')
-    reformatMGF_parser.add_argument('-i', '--input', help='Input MGF file')
-    reformatMGF_parser.add_argument('-o', '--output', help='Output new indexed MGF file')
+        description="De Novo Sequencing Pipeline for peptide prediction and assembly of full-protein sequences by"
+        "tandem mass spectrometry."
+    )
+    subparsers = parser.add_subparsers(dest="subparser_name")
+    reformatMGF_parser = subparsers.add_parser(
+        "reformatMGF",
+        help="Formatting MGF files to be compatible with all de novo tools. Indices are changed from 0 to N.",
+    )
+    reformatMGF_parser.add_argument("-i", "--input", help="Input MGF file")
 
-    denovosequencing_parser = subparsers.add_parser('denovo',
-                                                    help='Start the de novo sequencing analysis')
-    denovosequencing_parser.add_argument('-i', '--input', help='Input reformatted MGF file')
-    denovosequencing_parser.add_argument('-o', '--output', help='result directory')
-    denovosequencing_parser.add_argument('--denovogui', nargs='?', const=0, type=int,
-                                         help='Turn Novor sequencing on or off. 1: on, '
-                                              '0: off, default is 0')
-    denovosequencing_parser.add_argument('--smsnet', nargs='?', const=0, type=int,
-                                         help='Turn SMSNet sequencing on or off. 1: on, 0: off, default '
-                                              'is 0')
-    denovosequencing_parser.add_argument('--deepnovo', nargs='?', const=0, type=int,
-                                         help='Turn DeepNovo sequencing on or off. 1: on, 0: off, '
-                                              'default is 0')
-    denovosequencing_parser.add_argument('--pnovo3', nargs='?', const=0, type=int,
-                                         help='Turn pNovo3 sequencing on or off. 1: on, 0: off, default '
-                                              'is 0')
-    denovosequencing_parser.add_argument('--pointnovo', nargs='?', const=0, type=int,
-                                         help='Turn PointNovo sequencing on or off. 1: on, 0: off, default '
-                                              'is 0')
-    denovosequencing_parser.add_argument('--params', default="resources/DeNovoGUI-1.16.6/newparameter.par",
-                                         type=str,
-                                         help='Location of Parameter File for DeNovoCli. See documentation of '
-                                              'DeNovoGUI for building paramter. If none is given it will use '
-                                              'resources/DeNovoGUI-1.16.6/newparameter.par')
-    denovosequencing_parser.add_argument('--smsnet_model', default="resources/SMSNet/MassIVE_HCD/", type=str,
-                                         help='Location of Model for SMSNet. If none is given it will use '
-                                              'resources/SMSNet/MassIVE_HCD')
-    denovosequencing_parser.add_argument('--deepnovo_model',
-                                         default="MassIVE_HCD",
-                                         type=str,
-                                         help='Location of Model for DeepNovo (PNAS). If none is given it will use resources/DeepNovo_Antibody/MassIVE_HCD')
-    denovosequencing_parser.add_argument('--pointnovo_model', default="train", type=str,
-                                         help='Location of Model for PointNovo. Default: train/')
-    setupdenovo_parser = subparsers.add_parser('setup',
-                                               help='Downloads knapsack, pre-trained models and test data')
-    summary_parser = subparsers.add_parser('summary',
-                                           help='Create Summary File')
-    summary_parser.add_argument('-i', '--input', help='Input MGF file')
-    summary_parser.add_argument('-r', '--results', help='Result Directory containing results from single de novo tools')
-    summary_parser.add_argument('-db', '--dbreport', default="",
-                                help='Path to the exported PSM Report by PeptideShaker')
+    denovosequencing_parser = subparsers.add_parser(
+        "denovo", help="Start the de novo sequencing analysis"
+    )
+    denovosequencing_parser.add_argument(
+        "-i", "--input", help="Input reformatted MGF file"
+    )
+    denovosequencing_parser.add_argument("-o", "--output", help="result directory")
+    denovosequencing_parser.add_argument(
+        "--denovogui",
+        nargs="?",
+        const=0,
+        type=int,
+        help="Turn Novor sequencing on or off. 1: on, " "0: off, default is 0",
+    )
+    denovosequencing_parser.add_argument(
+        "--smsnet",
+        nargs="?",
+        const=0,
+        type=int,
+        help="Turn SMSNet sequencing on or off. 1: on, 0: off, default " "is 0",
+    )
+    denovosequencing_parser.add_argument(
+        "--deepnovo",
+        nargs="?",
+        const=0,
+        type=int,
+        help="Turn DeepNovo sequencing on or off. 1: on, 0: off, " "default is 0",
+    )
+    denovosequencing_parser.add_argument(
+        "--pnovo3",
+        nargs="?",
+        const=0,
+        type=int,
+        help="Turn pNovo3 sequencing on or off. 1: on, 0: off, default " "is 0",
+    )
+    denovosequencing_parser.add_argument(
+        "--pointnovo",
+        nargs="?",
+        const=0,
+        type=int,
+        help="Turn PointNovo sequencing on or off. 1: on, 0: off, default " "is 0",
+    )
+    denovosequencing_parser.add_argument(
+        "--params",
+        default="resources/DeNovoGUI-1.16.6/newparameter.par",
+        type=str,
+        help="Location of Parameter File for DeNovoCli. See documentation of "
+        "DeNovoGUI for building paramter. If none is given it will use "
+        "resources/DeNovoGUI-1.16.6/newparameter.par",
+    )
+    denovosequencing_parser.add_argument(
+        "--smsnet_model",
+        default="resources/SMSNet/MassIVE_HCD/",
+        type=str,
+        help="Location of Model for SMSNet. If none is given it will use "
+        "resources/SMSNet/MassIVE_HCD",
+    )
+    denovosequencing_parser.add_argument(
+        "--deepnovo_model",
+        default="MassIVE_HCD",
+        type=str,
+        help="Location of Model for DeepNovo (PNAS). If none is given it will use resources/DeepNovo_Antibody/MassIVE_HCD",
+    )
+    denovosequencing_parser.add_argument(
+        "--pointnovo_model",
+        default="train",
+        type=str,
+        help="Location of Model for PointNovo. Default: train/",
+    )
+    setupdenovo_parser = subparsers.add_parser(
+        "setup", help="Downloads knapsack, pre-trained models and test data"
+    )
+    summary_parser = subparsers.add_parser("summary", help="Create Summary File")
+    summary_parser.add_argument("-i", "--input", help="Input MGF file")
+    summary_parser.add_argument(
+        "-r",
+        "--results",
+        help="Result Directory containing results from single de novo tools",
+    )
+    summary_parser.add_argument(
+        "-db",
+        "--dbreport",
+        default="",
+        help="Path to the exported PSM Report by PeptideShaker",
+    )
 
-    convertForALPS_parser = subparsers.add_parser('assembly',
-                                                  help='Takes single results from DeNovoTools, parses them und runs ALPS for assembly')
-    convertForALPS_parser.add_argument('-i', '--input', help='path to summary.csv file')
-    convertForALPS_parser.add_argument('-k', '--kmer', help='Length of k-mers for ALPS, 6 or 7 is recommended', default=7)
-    convertForALPS_parser.add_argument('-c', '--contigs', help='Number of top contigs to use for assembly of ALPS', default=20)
-    convertForALPS_parser.add_argument('-q', '--quality-cutoff', help='Threshold of Quality Cutoff. All Peptides below this score will not be used for assembly.', default=50)
-    convertForALPS_parser.add_argument('-s', '--create-stats', help='Option to export Stats about Recall and Precision using the results from database search', default=True)
+    convertForALPS_parser = subparsers.add_parser(
+        "assembly",
+        help="Takes single results from DeNovoTools, parses them und runs ALPS for assembly",
+    )
+    convertForALPS_parser.add_argument("-i", "--input", help="path to summary.csv file")
+    convertForALPS_parser.add_argument(
+        "-k",
+        "--kmer",
+        help="Length of k-mers for ALPS, 6 or 7 is recommended",
+        default=7,
+    )
+    convertForALPS_parser.add_argument(
+        "-c",
+        "--contigs",
+        help="Number of top contigs to use for assembly of ALPS",
+        default=20,
+    )
+    convertForALPS_parser.add_argument(
+        "-q",
+        "--quality-cutoff",
+        help="Threshold of Quality Cutoff. All Peptides below this score will not be used for assembly.",
+        default=50,
+    )
+    convertForALPS_parser.add_argument(
+        "-s",
+        "--create-stats",
+        help="Option to export Stats about Recall and Precision using the results from database search",
+        default=True,
+    )
 
     args = parser.parse_args()
-    if args.subparser_name == 'setup':
+    if args.subparser_name == "setup":
         logger.info("Setup started")
         denovo_setup()
         sys.exit(0)
-    elif args.subparser_name == 'reformatMGF':
+    elif args.subparser_name == "reformatMGF":
         logger.info("Formatting of MGF started")
-        reformat_MGF(args.input, args.output)
+        reformat_MGF(args.input)
         sys.exit(0)
-    elif args.subparser_name == 'denovo':
+    elif args.subparser_name == "denovo":
         logger.info("De novo sequencing started")
-        if (args.input == None):
+        if args.input == None:
             denovosequencing_parser.print_help()
         else:
-            denovo_seq(args.input, args.output, args.denovogui, args.smsnet, args.deepnovo, args.pnovo3, args.pointnovo,
-                   args.params, args.smsnet_model, args.deepnovo_model, args.pointnovo_model)
+            denovo_seq(
+                args.input,
+                args.output,
+                args.denovogui,
+                args.smsnet,
+                args.deepnovo,
+                args.pnovo3,
+                args.pointnovo,
+                args.params,
+                args.smsnet_model,
+                args.deepnovo_model,
+                args.pointnovo_model,
+            )
         sys.exit(0)
-    elif args.subparser_name == 'summary':
+    elif args.subparser_name == "summary":
         logger.info("Generating summary file started.")
         from createsummary import denovo_summary
+
         denovo_summary(args.input, args.results, args.dbreport)
         sys.exit(0)
-    elif args.subparser_name == 'assembly':
+    elif args.subparser_name == "assembly":
         from assembly import convert_For_ALPS
+
         logger.info("Assembly started.")
-        convert_For_ALPS(args.input, args.kmer, args.contigs, args.quality_cutoff, args.create_stats)
+        convert_For_ALPS(
+            args.input, args.kmer, args.contigs, args.quality_cutoff, args.create_stats
+        )
         sys.exit(0)
     elif args.subparser_name == None:
         logger.info("Unspecified Mode.")
         parser.print_help()
         sys.exit(0)
-
-
-
-
